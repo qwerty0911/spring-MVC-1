b4d97e9540b61b4907da3b0d8640ff3696b4d84f
BK <qwerty0911@gachon.ac.kr>
2023-02-05 00:16:05 +0900

    Member repository 생성 및 test 생성

--- src/test/java/hello/servlet/ServletApplicationTests.java
+++ src/test/java/hello/servlet/ServletApplicationTests.java
@@ -1,13 +1,55 @@
 package hello.servlet;
 
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ServletApplicationTests {
 
+	MemberRepository memberRepository =MemberRepository.getInstance();
+
+	@AfterEach
+	void afterEach(){
+		memberRepository.clearStore();
+	}
+
 	@Test
-	void contextLoads() {
+	void save(){
+		//given
+		Member member = new Member("hello", 20);
+
+
+		//when
+		Member savedMember = memberRepository.save(member);
+
+
+		//then
+		Member findMember  = memberRepository.findById(savedMember.getId());
+		Assertions.assertThat(findMember).isEqualTo(savedMember);
+	}
+
+	@Test
+	void findAll(){
+		//given
+		Member member1 = new Member("member1",20);
+		Member member2 = new Member("member2",20);
+
+		memberRepository.save(member1);
+		memberRepository.save(member2);
+
+		//when
+		List<Member> result = memberRepository.findAll();
+
+		//then
+		Assertions.assertThat(result.size()).isEqualTo(2);
+
+
 	}
 
 }
